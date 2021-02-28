dfe419d64e06280ee995bb705fe3b8c81c8444a6
Trevor Martin <trevor.j.martin@gmail.com>
2021-02-27 23:03:32 -0500

    added TypeScript interfaces

--- src/sockets/space.ts
+++ src/sockets/space.ts
@@ -1,71 +1,153 @@
 import axios from "axios";
 
-const space: any = {
-  rooms: ["#general"],
-  stars: {},
-  "#general": { id: "#general", buffer: [], welcome: "welcome to #general" },
-  listeners: [
-    {
-      room: "*",
-      startswith: "/join",
-      lambda: (io: any, socket: any, msg: string) =>
-        io.to(socket.id).emit("!emit", "join", msg.slice(6))
-    },
-    {
-      room: "*",
-      startswith: "/list",
-      lambda: (io: any, socket: any, msg: string) =>
-        io.to(socket.id).emit("rooms", listRooms())
-    },
-    {
-      room: "*",
-      startswith: "/norris",
-      lambda: (io: any, socket: any, msg: string) => {
-        axios
-          .get("http://api.icndb.com/jokes/random?limitTo=[nerdy]")
-          .then((result: any) => {
-            messageRoom(io, socket, result.data.value.joke, "😂");
-          });
-      }
-    },
-    {
-      room: "*",
-      startswith: "/welcome",
-      lambda: (io: any, socket: any, msg: string) => {
-        messageRoom(io, socket, getWelcomeMessage(socket), "👋");
-      }
-    },
-    {
-      room: "*",
-      startswith: "/topic",
-      lambda: (io: any, socket: any, msg: string) => {
-        console.log(`${currentUsername(socket)} set the topic of ${currentRoom(socket)} to ${msg.slice(6)}`);
-        messageRoom(io, socket, setWelcomeMessage(socket, msg.slice(6)), "👋");
-      }
-    },
-    {
-      room: "*",
-      startswith: "/username",
-      lambda: (io: any, socket: any, msg: string) => {
-        const oldUsername = currentUsername(socket);
-        changeName(socket, msg.slice(10))
-        console.log(`${oldUsername} changed their name to ${currentUsername(socket)}`);
-        io.to(socket.id).emit("/username", msg.slice(10));
-      }
-    }
-  ]
+/**
+ * connect SOCKET.ID with UUID
+ */
+interface userMap {
+  [key: string]: string;
+}
+
+/**
+ * connect UUID with user information
+ */
+interface userLand {
+  id: string;
+  username: string;
+  current_room: string;
+  [key: string]: any;
+}
+
+/**
+ * /users in #room
+ */
+interface connectedList {
+  id: string;
+  name: string;
+}
+
+/**
+ * #room
+ */
+interface spaceRoom {
+  id: string;
+  buffer: any[];
+  welcome: string;
+  connected: connectedList[] | any[];
+  [key: string]: any;
+}
+
+/**
+ * listener
+ */
+interface lambdaListener {
+  room: string;
+  startswith: string;
+  lambda: any;
+  [key: string]: any;
+}
+
+/**
+ * #room-names
+ */
+const rooms: string[] = ["#general"];
+
+const stars: userMap = {};
+
+/**
+ * default room
+ */
+const general: spaceRoom = {
+  id: "#general",
+  buffer: [],
+  welcome: "welcome to #general",
+  connected: []
 };
-function changeName(socket:any, username:string) {
+
+interface memorySpace {
+  [key: string]: string[] | userMap | spaceRoom | userLand | any;
+}
+
+const listeners: lambdaListener[] = [
+  {
+    room: "*",
+    startswith: "/join",
+    lambda: (io: any, socket: any, msg: string) =>
+      io.to(socket.id).emit("!emit", "join", msg.slice(6))
+  },
+  {
+    room: "*",
+    startswith: "/list",
+    lambda: (io: any, socket: any, msg: string) =>
+      io.to(socket.id).emit("/rooms", listRooms())
+  },
+  {
+    room: "*",
+    startswith: "/norris",
+    lambda: (io: any, socket: any, msg: string) => {
+      axios
+        .get("http://api.icndb.com/jokes/random?limitTo=[nerdy]")
+        .then((result: any) => {
+          messageRoom(io, socket, result.data.value.joke, "😂");
+        });
+    }
+  },
+  {
+    room: "*",
+    startswith: "/welcome",
+    lambda: (io: any, socket: any, msg: string) => {
+      messageRoom(io, socket, getWelcomeMessage(socket), "👋");
+    }
+  },
+  {
+    room: "*",
+    startswith: "/topic",
+    lambda: (io: any, socket: any, msg: string) => {
+      console.log(
+        `${currentUsername(socket)} set the topic of ${currentRoom(
+          socket
+        )} to ${msg.slice(6)}`
+      );
+      messageRoom(io, socket, setWelcomeMessage(socket, msg.slice(6)), "👋");
+    }
+  },
+  {
+    room: "*",
+    startswith: "/username",
+    lambda: (io: any, socket: any, msg: string) => {
+      const oldUsername = currentUsername(socket);
+      changeName(socket, msg.slice(10));
+      console.log(
+        `${oldUsername} changed their name to ${currentUsername(socket)}`
+      );
+      io.to(socket.id).emit("/username", msg.slice(10));
+    }
+  },
+  {
+    room: "*",
+    startswith: "/users",
+    lambda: (io: any, socket: any, msg: string) =>
+      io.to(socket.id).emit("/users", listUsers(currentRoom(socket)))
+  }
+];
+
+const space: memorySpace = {
+  rooms,
+  stars,
+  "#general": general,
+  listeners
+};
+
+function changeName(socket: any, username: string) {
   if (!username) {
     return;
   }
   space[space.stars[socket.id]] = {
     ...space[space.stars[socket.id]],
     username
-  }
+  };
 }
 
-export function currentRoom(socket: any) {
+function currentRoom(socket: any) {
   return space[space.stars[socket.id]].current_room;
 }
 
@@ -97,7 +179,7 @@ function setWelcomeMessage(socket: any, msg: string | undefined) {
 
 function messageRoom(io: any, socket: any, message: string, sender: string) {
   const max = process.env.MAX_LINES ? Number(process.env.MAX_LINES) : 60;
-  const room = currentRoom(socket);
+  const room: string = currentRoom(socket);
   space[room].buffer.push(`${sender},${message}`);
   space[room].buffer = space[room].buffer.slice(
     space[room].buffer.length - max
@@ -107,8 +189,12 @@ function messageRoom(io: any, socket: any, message: string, sender: string) {
 
 function listRooms() {
   return space.rooms.map(
-    (room: string) => `(${space[room].connected.length}) ${room}`
+    (room: string) => `(${space[room]?.connected.length}) ${room}`
   );
+}
+
+function listUsers(room: string) {
+  return space[room].map((u: any) => u.name);
 }
 
 function matchWords(subject: string, words: any[]) {
@@ -123,4 +209,21 @@ function matchWords(subject: string, words: any[]) {
   return subject.match(regex) || [];
 }
 
-export default space;
+export {
+  space,
+  userMap,
+  userLand,
+  connectedList,
+  spaceRoom,
+  lambdaListener,
+  memorySpace,
+  listeners,
+  changeName,
+  currentRoom,
+  currentUsername,
+  getWelcomeMessage,
+  messageRoom,
+  listRooms,
+  listUsers,
+  matchWords
+};
